945176d8c1e7bcbab5a1c0feb5a39eb020981db3
itamar <itamarpetel2002@gmail.com>
2024-05-22 20:40:54 +0300

    i

--- components/FlipCard/index.jsx
+++ components/FlipCard/index.jsx
@@ -5,7 +5,7 @@ export default function FlipCard({ firstImg, secondImg }) {
             <div className={style.main}>
                 <div className={style.holdFlip}>
 
-                    <div className={style.front}><img style={{ width: "100%", height: "100%" }} src={firstImg} alt="" /></div>
+                    <div className={style.front}><img  style={{ width: "100%", height: "100%" ,borderRadius:"20px"}} src={firstImg} alt="" /></div>
                     <div style={{ backgroundImage: `url(${secondImg})` }} className={style.back}>
                         <div>
                             לחנות הרשמית
