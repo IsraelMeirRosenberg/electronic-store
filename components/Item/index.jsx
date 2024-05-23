4cee60e09010f1ecc7bbcf24dad3dde9446bf281
itamar <itamarpetel2002@gmail.com>
2024-05-23 17:31:09 +0300

    itamar

--- components/Item/index.jsx
+++ components/Item/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import style from "./style.module.scss";
 import BtnAddToCart from "../BtnAddToCart";
-export default function Item({...props}) {
-const {image , name} = props
-console.log(name);
+import Link from "next/link";
+export default function Item({image , name , price , id ,desc}) {
+
   const flags = {
     samsung:
       "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/attribute/swatch/s/a/samsung_logo.png",
@@ -12,26 +12,33 @@ console.log(name);
     LG: "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/attribute/swatch/l/g/lg_logo.png",
   };
   return (
+    <Link  href={`/item/${id}`} >
     <div className={style.main}>
       <div className={style.companyImg}>
         <img src={flags["LG"]} alt="" />
       </div>
       <div className={style.holdImg}>
         <img
-          src={id}
+          src={image}
           alt=""
-        />
+          />
       </div>
-      <p>
-        <b>צבע : שחור</b>
+      <div className={style.flexTexts}>
+
+      <p className={style.nameItem}>{name}</p>
+      <p className={style.textPrice}>
+        צבע : שחור
       </p>
       <div>
         {" "}
-        מקרר 4 דלתות בסגנון צרפתי 468 מקפיא תחתון פלוס כגימור נירוסטה אחריות
-        יבואן רשמי
+        {desc}
+        {/* מקרר 4 דלתות בסגנון צרפתי 468 מקפיא תחתון פלוס כגימור נירוסטה אחריות
+        יבואן רשמי */}
       </div>
-      <div>{price}</div>
+      <div className={style.textPrice}>₪{price}</div>
       <BtnAddToCart />
+        </div>
     </div>
+          </Link>
   );
 }
