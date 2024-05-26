9fa59da3a1b0f4bea4d850c93886f9d2c56ff3af
itamar <itamarpetel2002@gmail.com>
2024-05-26 21:10:05 +0300

    itamar

---
+++ data/flagsItem.js
@@ -0,0 +1,39 @@
+export const flagsItem = {
+  samsung:
+    "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/attribute/swatch/s/a/samsung_logo.png",
+  electra:
+    "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/attribute/swatch/e/l/electra_logo.png",
+  lg: "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/attribute/swatch/l/g/lg_logo.png",
+  sony: "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/Brands_Car_Homepage/Sony_logo.png"
+  ,
+  hp: "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/Brands_Car_Homepage/Hp_logo.png",
+  lenovo: "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/Brands_Car_Homepage/Lenovo_logo.png",
+  bosch: "https://d2d22nphq0yz8t.cloudfront.net/6cbcadef-96e0-49e9-b3bd-9921afe362db/www.payngo.co.il/media/Brands_Car_Homepage/Bosch_logo.png",
+  vivo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Vivo_logo_2019.svg/2560px-Vivo_logo_2019.svg.png",
+  realme: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Realme-realme-_logo_box-RGB-01_with_out_back_ground.svg/2560px-Realme-realme-_logo_box-RGB-01_with_out_back_ground.svg.png",
+  motorola: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZNbswmClKtop2Eo8Z9hONqo3JWZouC9QOwZWB0ruo&s",
+  zte: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/ZTE-logo.svg/400px-ZTE-logo.svg.png",
+  oppo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/OPPO_Logo.svg/1280px-OPPO_Logo.svg.png",
+  google: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Pixel_wordmark.svg/2560px-Pixel_wordmark.svg.png",
+  nokia: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nokia_wordmark.svg/1024px-Nokia_wordmark.svg.png"
+  , asus: "https://upload.wikimedia.org/wikipedia/commons/d/de/AsusTek-black-logo.png",
+  microsoft: "https://blogs.microsoft.com/wp-content/uploads/prod/2012/08/8867.Microsoft_5F00_Logo_2D00_for_2D00_screen.jpg",
+  acer: "tqOx9T86OCfyXKH+A3vl11AAAAAElFTkSuQmCC",
+  dell: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1200px-Dell_Logo.png",
+  razer: "https://1000logos.net/wp-content/uploads/2019/06/razer.jpg",
+  apple: "https://www.shutterstock.com/image-vector/galati-romania-april-29-2023-600nw-2295394661.jpg"
+  , huawei: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8dnFRvzLq6Ym9c0bDFQNzSR1Qqlj2QZrnamZmmhfp&s"
+  , xiaomi: "https://www.svgrepo.com/show/303629/xiaomi-1-logo.svg",
+  oneplus: "https://i2.wp.com/theschedio.com/wp-content/uploads/2020/03/OnePlus_LU_Black_RGB.png?fit=740%2C254&ssl=1",
+  amazon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png"
+  , kitchenaid: "https://pngimagesfree.com/wp-content/uploads/Kitchenaid-Logo-white-png@.png",
+  whirlpool: "https://upload.wikimedia.org/wikipedia/commons/9/95/Whirlpool_Corporation_Logo_%28as_of_2017%29.svg",
+  ge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq22zitfoNu7n_ShWQbxZ0bNX8TqD6RCFE4FnVQco0zA&s",
+  amazfit: "https://iconape.com/wp-content/files/zm/349948/svg/349948.svg",
+  suunto: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Suunto-Logo.svg/1200px-Suunto-Logo.svg.png",
+  ticwatch: "https://m.media-amazon.com/images/S/aplus-media-library-service-media/a3f56637-a888-4577-8e6b-77b127bcae0d.__CR0,0,315,145_PT0_SX315_V1___.jpg",
+  fossil: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Fossil_Group_logo.svg/1280px-Fossil_Group_logo.svg.png",
+  fitbit: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Fitbit_logo.svg/2560px-Fitbit_logo.svg.png",
+  withings: "https://upload.wikimedia.org/wikipedia/commons/1/13/Logo_withings_black.png",
+  garmin: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Garmin_logo_2006.svg/2560px-Garmin_logo_2006.svg.png"
+};
