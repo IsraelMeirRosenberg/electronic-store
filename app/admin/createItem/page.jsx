78ceafcb2167f0398adee7b6615c98a15f693ffa
itamar <itamarpetel2002@gmail.com>
2024-05-28 16:21:44 +0300

    i

--- app/admin/createItem/page.jsx
+++ app/admin/createItem/page.jsx
@@ -1,18 +1,43 @@
 import style from './style.module.scss'
-export default function(){
+export default function () {
 
-    return(
+    return (
         <>
-        <div className={style.page}>
-<form className={style.page} action="">
-    <input type="text" />
-    <input type="number" />
-    <input type="text" />
-    <input type="text" />
-    <input type="text" />
-    <input type="text" />
-</form>
-        </div>
+            <div className={style.page}>
+                <form className={style.form} action="">
+                    <div className={style.holdLabels}>
+                        <label >
+                            <p>שם מוצר:</p>
+                            <input className={style.input} type="text" />
+                        </label>
+
+                        <label >
+                            <p> מחיר:</p>
+                            <input className={style.input} type="number" />
+                        </label>
+                        <label >
+                            <p>תיאור מוצר:</p>
+                            <input className={style.input} type="text" />
+                        </label>
+                        <label >
+                            <p>תמונת מוצר:</p>
+                            <input className={style.input} type="text" />
+                        </label>
+                        <label>
+                            צבע:
+                            <select className={style.select}>
+                                <option value="">שחור</option>
+                                <option value="">כחול</option>
+                            </select>
+                        </label>
+
+                        <select className={style.select}>
+                            <option value="">Apple</option>
+                            <option value="">Sharp</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
